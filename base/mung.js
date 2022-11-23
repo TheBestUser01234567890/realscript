ef8fd641f58ecca99856c533133d64b19dc5e03d
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2022-11-23 15:47:37 +0200

    Creates mung.js: MUNGs until no good

---
+++ base/mung.js
@@ -0,0 +1,18 @@
+/*
+ * WARNING: MAY DECREASE PERFORMANCE
+ */
+
+// MUNGs until no good
+function mung(data){
+    mung();
+    data = null;
+};
+
+// MUNGs until no good, but console.logs data
+function mungConsole(data){
+    mungConsole();
+    data = null;
+    console.log(data);
+};
+// mung("I will not be printed");
+// mungConsole("I will be printed");
