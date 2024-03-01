77980e29a7cc24e184ff9c6544ecbba5943694e2
TheBestUser01234567890 <98957578+TheBestUser01234567890@users.noreply.github.com>
2024-03-01 13:29:03 +0200

    fixed oops problems

--- misc/math.js
+++ misc/math.js
@@ -5,11 +5,11 @@ let RSMath={
   "Sqrt12":Math.SQRT1_2,
   "Sqrt1_2":this.Sqrt12,
   "Ln10":Math.LN2,
-  "Log2E":Math.LOG2E
+  "Log2E":Math.LOG2E,
   "Log2_E":this.Log2E,
   "Log_2E":this.Log2E,
   "Log_2_E":this.Log2E,
-  "Log10E":Math.LOG10E
+  "Log10E":Math.LOG10E,
   "Log10_E":this.Log10E,
   "Log_10E":this.Log10E,
   "Log_10_E":this.Log10E,
